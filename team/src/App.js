568705356c4cb8c8e62d8579ead22717f2b5dd64
yoonahyun <skgus11232@naver.com>
2021-10-20 22:26:33 +0900

    todolist

---
+++ team/src/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './App.css';
+import TodoTemplate from './TodoTemplate';
+
+
+const App=()=>{
+  return<TodoTemplate>todo앱을 만들자</TodoTemplate>
+};
+
+
+
+export default App;
